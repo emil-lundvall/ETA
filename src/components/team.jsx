f9fec0f61085dc498e24f208a74e043971122ffe
Anton Persson <antonsomoza@hotmail.com>
2022-01-20 02:03:24 +0100

    Started on "Meet our team." page.
    CSS and HTML is created here. Soon I will complete the CSS part.
    
    HTML part done for now.

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -6,7 +6,54 @@ import './../css/component-css/team.css';
 function team() {
     return (
         <div className="default-section team" data-scroll-index="4">
-            <div className="container-md"></div>
+            <div className="default-inner-section">
+                <div className="team-text-container">
+                    <div className="team-text-title">
+                        <h1>Meet our team<b>.</b></h1>
+                    </div>
+                    <div className="team-text-under">
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+                    </div>
+                </div>
+                <div className="team-staff-container">
+                    <div className="team-staff-inner-container">
+                        <div className="team-staff-item">
+                            <div className="staff-row-one">
+                                <div className="staff-img"></div>
+                            </div>
+                            <div className="staff-row-two">
+                                <h1>Emil Lundvall</h1>
+                                <p>Ägare</p>
+                            </div>
+                            <div className="staff-row-three">
+                                <div className="social-container">
+                                    <div className="social-item" id="instagram"><a><i></i></a></div>
+                                    <div className="social-item" id="linkedin"><a><i></i></a></div>
+                                    <div className="social-item" id="facebook"><a><i></i></a></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="team-staff-item">
+                            <div className='staff-row-one'>
+                                <div className="staff-img"></div>
+                            </div>
+                            <div className="staff-row-two">
+                                <h1>Noah Karlberg</h1>
+                                <p>Del-ägare</p>
+                            </div>
+                            <div className="staff-row-three">
+                                <div className="social-container">
+                                    <div className="social-item" id="instagram"><a><i></i></a></div>
+                                    <div className="social-item active-social" id="linkedin"><a><i></i></a></div>
+                                    <div className="social-item" id="facebook"><a><i></i></a></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
